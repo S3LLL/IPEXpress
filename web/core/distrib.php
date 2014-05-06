8afa9f389edcb46ad2924b5a598d8540c70435b7
Ricain <jeanmercadier@live.com>
2014-05-05 20:50:43 -0400

    creation object distrib

--- web/core/distrib.php
+++ web/core/distrib.php
@@ -1,5 +1,21 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Jean
+ * Date: 05/05/2014
+ * Time: 16:38
+ */
 
-	echo "super la command new";
+class Distrib {
 
-?>
+    private $settings;
+
+    function __construct($name){
+
+    }
+
+    function bootscript(){
+
+    }
+
+} 
