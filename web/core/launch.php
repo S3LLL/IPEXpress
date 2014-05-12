dc401396ef6cecde711a282d7c6139038779af3b
Ricain <jeanmercadier@live.com>
2014-05-12 17:22:15 -0400

    pas grand chose

--- web/core/launch.php
+++ web/core/launch.php
@@ -16,7 +16,7 @@
 
 	$distrib = new Distrib($_GET["distrib"]);
 
-	$nfspath = json_decode(file_get_contents("/etc/ipexpress/settings.json"))->{"web-server"} . "nfs/mounted/";
+	$nfspath = json_decode(file_get_contents("/etc/ipexpress/settings.json"))->{"path"} . "nfs/mounted/";
 
 ?>
 
@@ -29,7 +29,7 @@ goto lauch
 
 :lauch
 	echo Lancement de <?php echo $distrib->getName() . "\n"; ?>
-	kernel dump.php?distrib=<?php echo $distrib->getFolder(); ?>&fichier=kernel root=/dev/nfs rw nfsroot=${snfs}:/home/jean/ipexpress/nfs/mounted/<?php echo $distrib->getFolder() . " " . $distrib->getBootArguments(); ?> || goto fail
+	kernel dump.php?distrib=<?php echo $distrib->getFolder(); ?>&fichier=kernel root=/dev/nfs rw nfsroot=${snfs}:<?php echo $nfspath . $distrib->getFolder() . " " . $distrib->getBootArguments(); ?> || goto fail
 	initrd dump.php?distrib=<?php echo $distrib->getFolder(); ?>&fichier=initrd || goto fail
-	echo arguments: root=/dev/nfs rw nfsroot=${snfs}:<?php echo $nfspath . $distrib->getFolder() . " " . $distrib->getBootArguments(); ?>
+	echo arguments: root=/dev/nfs rw nfsroot=${snfs}:<?php echo $nfspath . $distrib->getFolder() . " " . $distrib->getBootArguments() . "\n"; ?>
 	boot || goto fail
