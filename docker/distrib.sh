145802464a7443bb4df8edc523cd4236925f3950
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2015-04-29 12:01:56 -0400

    Docker file automatisation

---
+++ docker/distrib.sh
@@ -0,0 +1,15 @@
+mount -o loop /opt/IPEXpress/distrib/ubuntu/ubuntu-14.04.2-desktop-amd64.iso /mnt
+cp /mnt/casper/initrd.lz /mnt/casper/vmlinuz.efi /opt/IPEXpress/distrib/ubuntu
+touch /opt/IPEXpress/distrib/ubuntu/param.json
+
+cat << EOF > /opt/IPEXpress/distrib/ubuntu/param.json
+{
+   "name"   : "ubuntu",
+   "image"  : "ubuntu-14.04.2-desktop-amd64.iso",
+   "kernel" : "vmlinuz.efi",
+   "initrd" : "initrd.lz",
+   "args"   : "boot=casper netboot=nfs"
+}
+EOF
+umount /mnt
+
