72568f848a4013d941c31c0a87f43d914cec9d56
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2015-04-29 09:10:59 -0400

    fix buid script

--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,20 @@
 set -e
 
 sed -i 's/key_buffer/key_buffer_size/g' /etc/mysql/my.cnf 
-sed -i 's/sam-recover/myisam-recover-options/g' /etc/mysql/my.cnf 
+sed -i 's/myisam-recover/myisam-recover-options/g' /etc/mysql/my.cnf 
 chown -R mysql:mysql /var/lib/mysql
 
 mysql_install_db --user mysql > /dev/null
 
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-"toor"}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-"toor1"}
 MYSQL_DATABASE=${MYSQL_DATABASE:-"ipexpress"}
 MYSQL_USER=${MYSQL_USER:-"ipex"}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-"toor"}
 
 tfile=`mktemp`
 
-if [[ ! -f "$tfile" ]]; then
-	exit 1 
+if [ ! -f "$tfile" ] ; then
+exit 1 
 fi
 
 cat << EOF > $tfile
@@ -24,13 +24,14 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") where user = "root";
 EOF
 
-if [[ $MYSQL_DATABASE != "" ]]; then
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+if [ $MYSQL_DATABASE != "" ] ; then
+	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-    if [[ $MYSQL_USER != "" ]]; then
-        echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-    fi
+	if [ $MYSQL_USER != "" ]; then
+		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+	fi
 fi
+
 
 /usr/sbin/mysqld --bootstrap --verbose=0 $MYSQLD_ARGS < $tfile
 rm -f $tfile
